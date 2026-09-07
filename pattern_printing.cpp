dc23c3271950a5c083f231d9b9dd23cf979bd374
ratan1501 <ratandeepmishra34@gmail.com>
2026-09-08 01:22:06 +0530

    pattern added

--- pattern_printing.cpp
+++ pattern_printing.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 
-// pattern printing 
+// pattern printing (triangle)
+// int main() {
+//     for (int i=1; i<=5; i++){
+//         for (int j=1; j<=i; j++) {
+//             cout << " * ";
+//         }
+//         cout << endl; 
+//     }
+// }
+
+// printing rectangle (pattern)
 int main() {
-    for (int i=1; i<=5; i++){
-        for (int j=1; j<=i; j++) {
-            cout << " * ";
+    for(int i=0; i<3; i++) {
+        for(int j=0; j<5; j++){
+            cout << "* ";
         }
-        cout << endl; 
+        cout << endl;
     }
 }
