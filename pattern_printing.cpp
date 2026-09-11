0b4ed1faed62ede025f902b07e5774d07b036be8
ratan1501 <ratandeepmishra34@gmail.com>
2026-09-12 02:43:09 +0530

    added Hollow Rectangle using User Input

--- pattern_printing.cpp
+++ pattern_printing.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// pattern printing (triangle)
+// Triangle Pattern Printing
 // int main() {
 //     for (int i=1; i<=5; i++){
 //         for (int j=1; j<=i; j++) {
@@ -11,8 +11,7 @@ using namespace std;
 //     }
 // }
 
-// printing solid rectangle (user input)
-
+// Solid Rectangle using User Input
 // OUTER: i = 0
 //     INNER: j = 0 → print *
 //     INNER: j = 1 → print *
@@ -40,18 +39,65 @@ using namespace std;
 // OUTER: i = 3 → STOP
 
 
+// int main() {
+//     int n;
+//     cout << "Enter a Number: ";
+//     cin >> n;
+    
+//     for(int i=0; i<n; i++) {
+//         for(int j=0; j<n; j++) {
+//             cout << " * ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// Hollow Rectangle
+// int main(){
+//     int n;
+//     for(int row=0; row<3; row++) {
+//         if (row==0 || row==2){
+//             for(int i=0; i<5 ; i++) {
+//                 cout << "* ";
+//             }
+//         }
+//         else {
+//             cout << "* ";
+//             for(int i=0; i<3; i++) {
+//                 cout << "  ";
+//             }
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     } 
+// }
+
+
+// Hollow Rectangle using User Input
+
 int main (){
-    int n;
-    cout << "Enter a Number for print pattern: ";
-    cin >> n;
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++){
-            cout << " * ";
+    int rowCount, colCount;
+    cout << "Enter the number of rows: ";
+    cin >> rowCount;
+    cout << "Enter the number of coloumn: ";
+    cin >> colCount;
+
+    for (int row=0; row<rowCount; row++){
+        if(row==0 || row == rowCount-1) {
+            for(int col=0; col<colCount; col++) {
+                cout << "* ";
+            }
+        }
+        else{
+            cout << "* ";
+            
+            for(int i=0; i<colCount-2; i++) {
+                cout << "  ";
+            }
+            cout << "* ";
         }
         cout << endl;
     }
+    
 }
-
-
-
-
